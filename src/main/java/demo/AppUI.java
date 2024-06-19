90003d3466f1a9a621e98ff37cfb8bd1919c814d
celsocontim <celso.contim@unimedbh.com.br>
2024-06-19 14:43:43 -0300

    sadojinaoksnjdnosaf

--- src/main/java/demo/AppUI.java
+++ src/main/java/demo/AppUI.java
@@ -10,8 +10,7 @@ import com.vaadin.ui.themes.ValoTheme;
 //import com.vaadin.flow.component.dependency.JsModule;
 //import com.vaadin.flow.component.UI;
 
-import java.io.Serializable;
-import java.io.StringReader;
+//import java.io.Serializable;
 
 //@JsModule("./src/electron-bridge.js")
 @Push(transport = Transport.WEBSOCKET)
@@ -21,21 +20,19 @@ public class AppUI extends UI {
     protected void init(VaadinRequest request) {
         TextField nameField = new TextField();
         nameField.setCaption("Seu nome");
-        //callElectronUiApi();
-        Button button = new Button("Veja a magica", event -> {
-            //Runtime runtime = Runtime.getRuntime();
-            new Notification(
-                            "Hello " +
-                            nameField.getValue())
-                    .show(getPage());
-        });
+
+        Button button = new Button("Veja a magica", event ->
+                new Notification(
+                        "Ola " + nameField.getValue()
+                ).show(getPage())
+        );
 
         VerticalLayout content = new VerticalLayout();
         content.addComponents(nameField, button);
         setContent(content);
     }
 
-    private void callElectronUiApi(Serializable... args) {
+//    private void callElectronUiApi(Serializable... args) {
  //       UI.getCurrent().getPage().executeJs("callElectronUiApi($0)", args);
-    }
+//    }
 }
